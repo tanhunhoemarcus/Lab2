644111934ab8b4aae48b6f9ca5df9efb468cad9e
tanhunhoemarcus <mthh23.21@ichat.sp.edu.sg>
2022-10-27 16:44:12 +0800

    .

---
+++ bmi.py
@@ -0,0 +1,15 @@
+def calculate_bmi(height, weight):
+  print("Height = " + str(height))
+  print("Weight = " + str(weight))
+
+  bmi = weight / (height * height)
+  print("BMI = "+str(bmi))
+  if bmi < 18.5:
+    print("Underweight!")
+  elif bmi <= 25.0:
+    print("Normal weight!")
+  else:
+    print("Overweight!")
+
+
+calculate_bmi(weight=57, height=1.43)
